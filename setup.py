46cde6512cda0229ea589823ad0f21293fd1849e
Julian Reul <j.reul@fz-juelich.de>
2023-02-10 14:26:08 +0100

    Updated .yaml-file for env-definition

--- setup.py
+++ setup.py
@@ -8,5 +8,17 @@ setup(name='mode_behave',
       url='https://github.com/julianreul/mode_behave',
       license='MIT',
       packages=['mode_behave_public'],
+      install_requires=[
+          'Pillow>=9.4.0',
+          'matplotlib>=3.5.1',
+          'numba>=0.55.1',
+          'numpy>=1.21.5',
+          'pandas>=1.4.2',
+          'pickleshare>=0.7.5',
+          'scikit-learn>=1.0.2',
+          'scikit-optimize>=0.9.0',
+          'scipy>=1.7.3',
+          'seaborn>=0.11.2'
+          ],    
       include_package_data=True,
       zip_safe=False)
