e5d3e1d5b9780e6e904d8fd2d8e849f35825bd67
julian_paul <julian.reul@rwth-aachen.de>
2023-10-08 11:18:34 +0200

    Changed requirements to fit future evolvements

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='mode_behave',
-      version='1.1.1',
+      version='1.1.2',
       description='Estimation and simulation of discrete choice models',
       author='Julian Reul',
       author_email='j.reul@fz-juelich.de',
@@ -9,16 +9,16 @@ setup(name='mode_behave',
       license='MIT',
       packages=['mode_behave_public'],
       install_requires=[
-          'Pillow==9.5.0',
-          'matplotlib==3.7.1',
-          'numba==0.56.4',
-          'numpy==1.23.5',
-          'pandas==2.0.1',
-          'pickleshare==0.7.5',
-          'scikit-learn==1.2.2',
-          'scikit-optimize==0.9.0',
-          'scipy==1.10.1',
-          'seaborn==0.12.2'
+          'Pillow>=9.5.0',
+          'matplotlib>=3.7.1',
+          'numba>=0.56.4',
+          'numpy>=1.23.5',
+          'pandas>=2.0.1',
+          'pickleshare>=0.7.5',
+          'scikit-learn>=1.2.2',
+          'scikit-optimize>=0.9.0',
+          'scipy>=1.10.1',
+          'seaborn>=0.12.2'
           ],    
       include_package_data=True,
       zip_safe=False)
